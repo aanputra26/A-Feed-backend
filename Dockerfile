5f62df47fb7679d03389a482a5998f6d4908b11b
Aan Putra Bungsu <aanesiyaa@gmail.com>
2024-06-19 20:32:16 +0700

    all oke

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,14 @@ FROM node:18-buster
 
 WORKDIR /app
 
-ENV PORT 5000
+ENV ACCESS_TOKEN_SECRET=hdkasdhioehadadnakdnskdhasn8a98yd9adha
+ENV REFRESH_TOKEN_SECRET = jkjhdoidhaohdod89679y578dihdkd6766443hhjdk
+
+ENV DB_USER=root
+ENV DB_PASS=123456
+ENV DB_HOST=34.50.66.133
+ENV DB_NAME=auth_capstone
+ENV DB_DIALECT=mysql
 
 COPY . .
 
--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,19 @@
 import { Sequelize } from 'sequelize';
+import "dotenv/config";
 
-const db = new Sequelize('auth_capstone', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+// const db = new Sequelize('auth_capstone', 'root', '', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+// });
+
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
 export default db;
